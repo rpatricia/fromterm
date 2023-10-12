5390e5c0e8011278c331d65831ae9172d627254c
rafasdiass <128534833+rafasdiass@users.noreply.github.com>
2023-10-12 18:52:42 -0300

    quem somos

--- src/app/component/fromtherm/fromtherm.component.ts
+++ src/app/component/fromtherm/fromtherm.component.ts
@@ -6,5 +6,30 @@ import { Component } from '@angular/core';
   styleUrls: ['./fromtherm.component.scss']
 })
 export class FromthermComponent {
+  showTimeline: boolean = true; 
+  foundationYear: number = 2020;
+  currentYear: number = new Date().getFullYear();
+  yearsSinceFoundation: number = this.currentYear - this.foundationYear;
+  imageList: string[] = ["https://via.placeholder.com/500", "https://via.placeholder.com/500/111", "https://via.placeholder.com/500/222"];
+  currentImageIndex: number = 0;
 
+  // Linha do tempo com marcos da empresa
+  timelineEvents = [
+    { year: 2020, event: "Fundação da Fromtherm." },
+    { year: 2021, event: "Lançamento do primeiro produto principal." },
+    { year: 2022, event: "Expansão para mercados internacionais." }
+    // Adicione mais marcos conforme necessário
+  ];
+
+  toggleTimeline(): void {
+    this.showTimeline = !this.showTimeline;
+  }
+
+  changeImage(): void {
+    this.currentImageIndex = (this.currentImageIndex + 1) % this.imageList.length;
+  }
+
+  getCurrentImage(): string {
+    return this.imageList[this.currentImageIndex];
+  }
 }
